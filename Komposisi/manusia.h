7cfa7552c0036ddde48188c8f386bb3a3a725808
HafizZada16 <hafizzada16@gmail.com>
2024-05-21 18:28:37 +0700

    Add class manusia

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -2,6 +2,18 @@
 #define MANUSIA_H
 using namespace std;
 
+class manusia {
+public: 
+    string name;
+    jantung varjantung;
 
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+        }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
 
 #endif
