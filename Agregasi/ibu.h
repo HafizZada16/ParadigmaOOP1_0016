e118bb21e94465c43fb03b4013ae9ba5b7a805ca
HafizZada16 <hafizzada16@gmail.com>
2024-05-21 20:03:36 +0700

    Add void Ibu (cetakAnak)

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -19,5 +19,12 @@ public:
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[1]->nama << endl;
+    }
+    cout << endl;
+}
 
 #endif
